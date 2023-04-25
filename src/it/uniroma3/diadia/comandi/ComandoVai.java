85b6ca47c1ffabc332ffdc65c981cc8189819f57
MarcoBen17 <mar.benincasa1@stud.uniroma3.it>
2023-04-25 12:38:14 +0200

    DiaDia ver. SecondoHW

--- src/it/uniroma3/diadia/comandi/ComandoVai.java
+++ src/it/uniroma3/diadia/comandi/ComandoVai.java
@@ -4,10 +4,10 @@ import it.uniroma3.diadia.ambienti.Stanza;
 
 public class ComandoVai implements Comando {
 	private String direzione;
-	
+
 	@Override
 	public void esegui(Partita partita) {
-		
+
 		Stanza stanzaCorrente= partita.getStanzaCorrente();
 		Stanza prossimaStanza= null;
 		if (this.direzione== null) {
@@ -15,7 +15,7 @@ public class ComandoVai implements Comando {
 			return;
 		}
 		prossimaStanza= stanzaCorrente.getStanzaAdiacente(this.direzione);
-		
+
 		if (prossimaStanza== null) {
 			partita.getIO().mostraMessaggio("Direzione inesistente");
 			return;
@@ -23,19 +23,19 @@ public class ComandoVai implements Comando {
 		partita.setStanzaCorrente(prossimaStanza);
 		partita.getIO().mostraMessaggio(partita.getStanzaCorrente().getNome());
 		partita.getGiocatore().setCfu(partita.getGiocatore().getCfu()-1);
-		
+
 	}
-	
+
 	@Override
 	public void setParametro(String parametro) {
 		this.direzione= parametro;
 	}
-	
+
 	@Override
 	public String getNome() {
 		return "vai";
 	}
-	
+
 	@Override
 	public String getParametro() {
 		return this.direzione;
--- test/it/uniroma3/diadia/IOSimulatorTest.java
+++ test/it/uniroma3/diadia/IOSimulatorTest.java
@@ -19,7 +19,22 @@ class IOSimulatorTest {
 	}
 	
 	@Test
-	void testNessunComando() {
+	void testPartitaSimulata() {
+		DiaDia diadia;
+		String messaggioDiBenvenuto = ""+
+				"Ti trovi nell'Universita', ma oggi e' diversa dal solito...\n" +
+				"Meglio andare al piu' presto in biblioteca a studiare. Ma dov'e'?\n"+
+				"I locali sono popolati da strani personaggi, " +
+				"alcuni amici, altri... chissa!\n"+
+				"Ci sono attrezzi che potrebbero servirti nell'impresa:\n"+
+				"puoi raccoglierli, usarli, posarli quando ti sembrano inutili\n" +
+				"o regalarli se pensi che possano ingraziarti qualcuno.\n\n"+
+				"Per conoscere le istruzioni usa il comando 'aiuto'.";
 		
+		String[] stampe= {messaggioDiBenvenuto ,"Aula N10", "attrezzo raccolto"};
+		
+		IOSimulator iosimulator = new IOSimulator(stampe,"vai sud", "prendi lanterna");
+		diadia= new DiaDia(iosimulator);
+		diadia.gioca();
 	}
 }
