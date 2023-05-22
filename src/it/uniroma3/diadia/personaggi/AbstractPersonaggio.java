4a9d1b74ee96019c6414b5b6c7024babf966566c
MarcoBen17 <mar.benincasa1@stud.uniroma3.it>
2023-05-22 18:58:13 +0200

    pre sirio parte2

--- src/it/uniroma3/diadia/personaggi/AbstractPersonaggio.java
+++ src/it/uniroma3/diadia/personaggi/AbstractPersonaggio.java
@@ -4,6 +4,7 @@ public abstract class AbstractPersonaggio {
 
 	private String nome;
 	private String descrizione;
+
 	private boolean haSalutato;
 	
 	public AbstractPersonaggio(String nome, String descrizione) {
@@ -12,5 +13,23 @@ public abstract class AbstractPersonaggio {
 		this.haSalutato= false;
 	}
 	
+	public String getNome() {
+		return nome;
+	}
 	
+	public abstract void agisci();
+	
+	public boolean haSalutato() {
+		return this.haSalutato;
+	}
+	
+	public void saluta() {
+		StringBuilder presentazione= new StringBuilder();
+		
+	}
+	
+	@Override
+	public String toString() {
+		return this.getNome();
+	}
 }
